b39464a7ac8f60e391eab876158613d88e7a80a6
kuman514 <7iggif@naver.com>
2022-01-30 00:22:41 +0900

    Changed App design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Title from './components/Title';
+import Board from './components/Board';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Title />
+      <Board />
     </div>
   );
 }

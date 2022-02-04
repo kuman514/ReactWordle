0931c51abc8885a3d6ca89dc33a541163e7840a7
kumankoishi.py <7iggif@naver.com>
2022-02-05 00:00:16 +0900

    Added checking the word in the wordlist

--- src/contents/WordPicker.js
+++ src/contents/WordPicker.js
@@ -12,3 +12,13 @@ const [questionNumber, answer] = pickWord();
 
 export const QUESTION_NUMBER = questionNumber;
 export const ANSWER = answer;
+
+const wordListSet = new Set(wordList);
+
+export function isInWordList(word) {
+  if (wordListSet.has(word)) {
+    return true;
+  }
+  
+  return false;
+}

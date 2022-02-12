916df5cc2879616b55a391c37d608397d580d243
kumankoishi.py <7iggif@naver.com>
2022-02-13 00:09:26 +0900

    Made Line shows Tile components

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,24 +1,21 @@
+import Tile from './Tile';
+
 function Line(props) {
   /*
     Props of Line
-      - try: number
-      - submitted: boolean
-      - input: string (length up to 5)
-      - submitResult: stirng[] (length exactly 5)
+      - lineNo: number
   */
 
   return (
     <div className="Line">
       {
         [0, 1, 2, 3, 4].map((index) => {
-          const item = props.input[index] ? props.input[index] : '';
           return (
-            <span
-              key={`input_${props.try}-${index}`}
-              className={`Tile ${props.submitted ? props.submitResult[index] : ''}`}
-            >
-              { item }
-            </span>
+            <Tile
+              key={`input_${props.lineNo}-${index}`}
+              lineNo={props.lineNo}
+              colNo={index}
+            />
           );
         })
       }

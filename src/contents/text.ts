04f9dfcef6f2b6ac2392e9f8353b1db83ea6566e
kumankoishi.py <7iggif@naver.com>
2023-11-15 12:01:08 +0900

    chore: redirect old gh-pages link to vercel

--- src/contents/text.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  notLoaded: 'Word list not loaded.',
-  shortLength: 'Not enough letters.',
-  invalid: 'Not in word list.',
-  alreadyFinished: 'This game has been already finished.',
-};
--- src/env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_GET_WORDS_ENDPOINT: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
